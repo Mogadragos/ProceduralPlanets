b0c762b4cc70834688120171dcd9678c02d43d2f
Mogador <julien.caillaud.auditeur@lecnam.net>
2022-05-11 16:57:10 +0200

    Noise V0

--- Assets/Scripts/ShapeGenerator.cs
+++ Assets/Scripts/ShapeGenerator.cs
@@ -3,14 +3,40 @@ using UnityEngine;
 public class ShapeGenerator
 {
     ShapeSettings shapeSettings;
+    NoiseFilter[] noiseFilters;
 
     public ShapeGenerator(ShapeSettings shapeSettings)
     {
         this.shapeSettings = shapeSettings;
+        noiseFilters = new NoiseFilter[shapeSettings.noiseLayers.Length];
+        for(int i = 0; i < noiseFilters.Length; i++)
+        {
+            noiseFilters[i] = new NoiseFilter(shapeSettings.noiseLayers[i].noiseSettings);
+        }
     }
 
     public Vector3 CalculatePointOnPlanet(Vector3 pointOnUnitSphere)
     {
-        return pointOnUnitSphere * shapeSettings.planetRadius;
+        float firstLayerValue = 0;
+        float elevation = 0;
+
+        if(noiseFilters.Length > 0)
+        {
+            firstLayerValue = noiseFilters[0].Evaluate(pointOnUnitSphere);
+            if(shapeSettings.noiseLayers[0].enabled)
+            {
+                elevation = firstLayerValue;
+            }
+        }
+
+        for(int i = 1; i < noiseFilters.Length; i++)
+        {
+            if(shapeSettings.noiseLayers[i].enabled)
+            {
+                float mask = (shapeSettings.noiseLayers[i].useFirstLayerAsMask) ? firstLayerValue : 1;
+                elevation += noiseFilters[i].Evaluate(pointOnUnitSphere) * mask;
+            }
+        }
+        return (1 + elevation) * shapeSettings.planetRadius * pointOnUnitSphere;
     }
 }
