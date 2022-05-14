ddfcd70b1fa070581c6105f81750eb607ecbed60
Edralos <32432267+Edralos@users.noreply.github.com>
2022-05-14 16:07:54 +0200

    Soleil & main scene configurés

---
+++ Assets/Scripts/SolarSystemBehaviour.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SolarSystemBehaviour : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public List<Transform> Planets;
+    List<float> RevolutionTimes = new List<float>();
+
+    void Start()
+    {
+                
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
