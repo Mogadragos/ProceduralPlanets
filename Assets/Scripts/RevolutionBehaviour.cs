ffc47047b9be8fd7ca13a659cf78a4366c8133d9
Edralos <32432267+Edralos@users.noreply.github.com>
2022-05-14 18:28:10 +0200

    spawn planétaire

--- Assets/Scripts/RevolutionBehaviour.cs
+++ Assets/Scripts/RevolutionBehaviour.cs
@@ -12,7 +12,17 @@ public class RevolutionBehaviour : MonoBehaviour
     void Start()
     {
         degreesPerSecond = Random.Range(0.5f, 5f);
-        Planet.transform.localPosition = new Vector3(Random.Range(10f, 50f), 0, 0f);
+        float x = Random.Range(-1f, 1f);
+        float z = Random.Range(-1f, 1f);
+        if (x > 0)
+            x = 1;
+        else
+            x=-1;
+        if (z > 0)
+            z = 1;
+        else
+            z=-1;
+        Planet.transform.localPosition = new Vector3(Random.Range(5f, 30f)*x, 0f, Random.Range(5f, 30f) * z);
     }
 
     // Update is called once per frame
