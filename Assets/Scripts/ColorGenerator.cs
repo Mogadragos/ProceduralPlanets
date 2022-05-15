4e298d872c948ffe9756ef206c58d5e8ffb922fc
Mogador <julien.caillaud.auditeur@lecnam.net>
2022-05-15 14:23:44 +0200

    Commentaires couleur

--- Assets/Scripts/ColorGenerator.cs
+++ Assets/Scripts/ColorGenerator.cs
@@ -2,38 +2,51 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+// Gestionnaire des couleurs de la planète
 public class ColorGenerator
 {
-    // Pour le shader
+    // Shader
     public Material material { get; private set; }
     Texture2D texture;
     const int textureResolution = 50;
 
-    // Couches de couleurs aléatoires
+    // Couleurs aléatoires
+
     Gradient gradient;
     GradientColorKey[] colorKey;
     GradientAlphaKey[] alphaKey;
 
     // Teinte générale (couleur plus froide / plus chaude)
+
     Color tint;
     float tintPercent = .2f;
 
     public ColorGenerator(float relativeDistanceToSun)
     {
+        // On crée un Material et une Texture pour notre planète
         material = new Material(Shader.Find("Shader Graphs/Planet"));
         texture = new Texture2D(textureResolution, 1);
-
+        
+        // On crée un Gradient qui sera remplit aléatoirement
         gradient = new Gradient();
-        // Populate the alpha  keys at relative time 0 and 1  (0 and 100%)
+
+        // L'alpha est toujours fixé à 1
         alphaKey = new GradientAlphaKey[2];
         alphaKey[0].alpha = 1.0f;
         alphaKey[0].time = 0.0f;
         alphaKey[1].alpha = 1.0f;
         alphaKey[1].time = 1.0f;
+
+        // On génère les couleurs aléatoirement
         RandomColor();
 
+        /* La teinte est proportionnelle à la distance au soleil :
+            Proche du soleil : rouge
+            Loin du soleil : bleue
+        */
         tint = Color.Lerp(new Color32(187, 11, 11, 255), new Color32(192, 223, 239, 255), relativeDistanceToSun);
 
+        // Plus on est proche des extrémités, plus la teinte à un impact sur la couleur de la planète
         if(relativeDistanceToSun > .5f)
         {
             tintPercent = Mathf.Lerp(0, .3f, (relativeDistanceToSun - .5f) * 2);
@@ -44,33 +57,35 @@ public class ColorGenerator
         }
     }
 
+    // Génération des couleurs aléatoires
     void RandomColor()
     {
-        // Populate the color keys at the relative time 0 and 1 (0 and 100%)
         colorKey = new GradientColorKey[7];
-        colorKey[0].color = Random.ColorHSV(); // new Color32(0, 10, 154, 255);
+        colorKey[0].color = Random.ColorHSV();
         colorKey[0].time = 0.0f;
-        colorKey[1].color = Random.ColorHSV(); // new Color32(158, 121, 8, 255);
+        colorKey[1].color = Random.ColorHSV();
         colorKey[1].time = .041f;
-        colorKey[2].color = Random.ColorHSV(); // new Color32(16, 152, 0, 255);
+        colorKey[2].color = Random.ColorHSV();
         colorKey[2].time = .126f;
-        colorKey[3].color = Random.ColorHSV(); // new Color32(10, 99, 0, 255);
+        colorKey[3].color = Random.ColorHSV();
         colorKey[3].time = .226f;
-        colorKey[4].color = Random.ColorHSV(); // new Color32(118, 51, 0, 255);
+        colorKey[4].color = Random.ColorHSV();
         colorKey[4].time = .441f;
-        colorKey[5].color = Random.ColorHSV(); // new Color32(79, 35, 0, 255);
+        colorKey[5].color = Random.ColorHSV();
         colorKey[5].time = .685f;
-        colorKey[6].color = Random.ColorHSV(); // Color.white;
+        colorKey[6].color = Random.ColorHSV();
         colorKey[6].time = 1.0f;
 
         gradient.SetKeys(colorKey, alphaKey);
     }
 
+    // Envoi du paramètre d'élévation au shader
     public void UpdateElevation(MinMax elevationMinMax)
     {
         material.SetVector("_elevationMinMax", new Vector4(elevationMinMax.Min, elevationMinMax.Max));
     }
 
+    // Mise à jour de la texture avec les nouvelles couleurs et envoi de celle-ci au shader
     public void UpdateColors()
     {
         Color[] colors = new Color[textureResolution];
